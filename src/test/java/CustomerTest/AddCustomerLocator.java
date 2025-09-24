4fa02bed340f0da7e05345d23fe2f03d9d87442f
xuanthu9x <1999xuanthu.com@gmail.com>
2025-09-24 22:28:17 +0700

    Add and edit test case add customer

--- src/test/java/CustomerTest/AddCustomerLocator.java
+++ src/test/java/CustomerTest/AddCustomerLocator.java
@@ -8,22 +8,23 @@ public class AddCustomerLocator {
     public static String inputVAT = "//input[@id = 'vat']";
     public static String inputPhone = "//input[@id = 'phonenumber']";
     public static String inputWebsite = "//input[@id = 'website']";
-    public static String groupDropdown = "//label[normalize-space()='Groups']/following::select[@id='groups_in[]']";
+    public static String groupDropdown = "//button[@data-id='groups_in[]']";
     public static String inputSearchGroup = "//button[@data-id='groups_in[]']/following::input[@aria-controls='bs-select-1']";
     public static String btnSelectAllGroup = "//button[@data-id='groups_in[]']/following::button[normalize-space()='Select All']";
     public static String btDeselectAllGroup = "//button[@data-id='groups_in[]']/following::button[normalize-space()='Deselect All']";
+    public static String optionGroup = "//div[@id='bs-select-1']/descendant::span[contains(normalize-space(),'java')]";
     public static String currencyDropdown = "//button[@data-id='default_currency']";
     public static String inputSearchCurrency="//input[@aria-controls='bs-select-2']";
     public static String currencyOptionUSD="//div[@id='bs-select-2']/descendant::span[contains(text(),'USD')]";
     public static String defaultLanguageDropdown = "//button[@data-id='default_language']";
     public static String defaultLanguageEnglish = "//div[@id = 'bs-select-3']/descendant::span[contains(text(),'English')]";
-    public static String addressTextArea = "///textarea[@id = 'address']";
+    public static String addressTextArea = "//textarea[@id = 'address']";
     public static String inputCity = "//input[@id = 'city']";
     public static String inputState = "//input[@id = 'state']";
     public static String inputZipCode = "//input[@id = 'zip']";
     public static String countryDropdown = "//button[@data-id='country']";
     public static String inputSearchCountry = "//button[@data-id='country']/following::input[@aria-controls='bs-select-4']";
-    public static String optionSearchCountry = "//div[@id='bs-select-4']/descendant::span[contains(text(),'Afghanistan')]";
+    public static String countryOption = "//div[@id='bs-select-4']/descendant::span[contains(text(),'United States')]";
     public static String btnSaveAndCreateContract = "//button[contains(@class,'save-and-add-contact')]";
     public static String btnOnlySave = "//button[contains(@class,'only-save')]";
 }
--- src/test/java/CustomerTest/AddCustomerTest.java
+++ src/test/java/CustomerTest/AddCustomerTest.java
@@ -3,6 +3,7 @@ package CustomerTest;
 import LoginTest.BassTest;
 import LoginTest.Common;
 import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -14,24 +15,111 @@ public class AddCustomerTest extends BassTest {
         Common.Login();
         Common.click(By.xpath(AddCustomerLocator.menuCustomer));
         Common.click(By.xpath(AddCustomerLocator.btnNewCustomer));
-        Common.sendKey(By.xpath(AddCustomerLocator.inputCompany),"T_Test_2209");
+        String companyName ="T_Test1_2409";
+        Common.sendKey(By.xpath(AddCustomerLocator.inputCompany),companyName);
         Common.click(By.xpath(AddCustomerLocator.btnOnlySave));
         Thread.sleep(3000);
-        Boolean companyNameDisplay = driver.findElement(By.xpath("//span[contains(text(),'T_Test_2209')]")).isDisplayed();
+        Boolean companyNameDisplay = driver.findElement(By.xpath("//span[contains(text(),'"+companyName+"')]")).isDisplayed();
         Assert.assertTrue(companyNameDisplay,"Add customer not successfully");
     }
     @Test
-    public void AddCustomerWithFullInfor(){
+    public void AddCustomerWithFullInfor() throws InterruptedException {
+        Common common = new Common(driver);
+        Common.Login();
+        Common.click(By.xpath(AddCustomerLocator.menuCustomer));
+        Common.click(By.xpath(AddCustomerLocator.btnNewCustomer));
 
+        String companyname = "T_Test2_2409";
+        Common.sendKey(By.xpath(AddCustomerLocator.inputCompany),companyname);
+        Common.sendKey(By.xpath(AddCustomerLocator.inputVAT),"VAT123");
+        Common.sendKey(By.xpath(AddCustomerLocator.inputPhone),"0336775288");
+        Common.sendKey(By.xpath(AddCustomerLocator.inputWebsite),"google.com");
+        Common.click(By.xpath(AddCustomerLocator.groupDropdown));
+        String group = "java";
+        Common.sendKey(By.xpath(AddCustomerLocator.inputSearchGroup),group);
+        Thread.sleep(1000);
+        Common.click(By.xpath("//div[@id='bs-select-1']/descendant::span[contains(normalize-space(),'"+group+"')]"));
+        driver.findElement(By.xpath("//body")).click();
+
+        Actions action = new Actions(driver);
+        action.moveToElement(driver.findElement(By.xpath(AddCustomerLocator.countryDropdown))).perform();
+
+        Common.click(By.xpath(AddCustomerLocator.currencyDropdown));
+        Common.click(By.xpath(AddCustomerLocator.currencyOptionUSD));
+        Common.click(By.xpath(AddCustomerLocator.defaultLanguageDropdown));
+        Common.click(By.xpath(AddCustomerLocator.defaultLanguageEnglish));
+        Common.sendKey(By.xpath(AddCustomerLocator.addressTextArea),"8414 Martin Luther King Jr Way South Seattle Washington 98118");
+        Common.sendKey(By.xpath(AddCustomerLocator.inputCity),"Washington ");
+        Common.sendKey(By.xpath(AddCustomerLocator.inputState),"Seattle ");
+        Common.sendKey(By.xpath(AddCustomerLocator.inputZipCode),"98118");
+        Common.click(By.xpath(AddCustomerLocator.countryDropdown));
+        Common.click(By.xpath(AddCustomerLocator.countryOption));
+
+        action.moveToElement(driver.findElement(By.xpath(AddCustomerLocator.btnOnlySave))).perform();
+        Common.click(By.xpath(AddCustomerLocator.btnOnlySave));
+        Thread.sleep(3000);
+        Boolean companyNameDisplay = driver.findElement(By.xpath("//span[contains(text(),'"+companyname+"')]")).isDisplayed();
+        Assert.assertTrue(companyNameDisplay,"Add customer not successfully");
     }
 
     @Test
-    public void AddCustomerAndCreateContactWithOnlyRequiredField(){
+    public void AddCustomerAndCreateContactWithOnlyRequiredField() throws InterruptedException {
+        Common common = new Common(driver);
 
+        Common.Login();
+        Common.click(By.xpath(AddCustomerLocator.menuCustomer));
+        Common.click(By.xpath(AddCustomerLocator.btnNewCustomer));
+        String companyName = "T_Test3_2409";
+        Common.sendKey(By.xpath(AddCustomerLocator.inputCompany),companyName);
+        Common.click(By.xpath(AddCustomerLocator.btnSaveAndCreateContract));
+        Thread.sleep(2000);
+        Boolean newContactPopup = driver.findElement(By.xpath("//div[@id='contact']")).isDisplayed();
+        Assert.assertTrue(newContactPopup,"Add new contact pop-up is not shown");
+        Common.click(By.xpath("//button[normalize-space() = 'Close']"));
+        Boolean companyNameDisplay = driver.findElement(By.xpath("//span[contains(text(),'"+companyName+"')]")).isDisplayed();
+        Assert.assertTrue(companyNameDisplay,"Add customer not successfully");
     }
     @Test
-    public void AddCustomerAndCreateContactWithFullInfo(){
+    public void AddCustomerAndCreateContactWithFullInfo() throws InterruptedException {
+        Common common = new Common(driver);
+        Common.Login();
+        Common.click(By.xpath(AddCustomerLocator.menuCustomer));
+        Common.click(By.xpath(AddCustomerLocator.btnNewCustomer));
 
+        String companyname = "T_Test4_2409";
+        Common.sendKey(By.xpath(AddCustomerLocator.inputCompany),companyname);
+        Common.sendKey(By.xpath(AddCustomerLocator.inputVAT),"VAT123");
+        Common.sendKey(By.xpath(AddCustomerLocator.inputPhone),"0336775288");
+        Common.sendKey(By.xpath(AddCustomerLocator.inputWebsite),"google.com");
+        Common.click(By.xpath(AddCustomerLocator.groupDropdown));
+        String group = "java";
+        Common.sendKey(By.xpath(AddCustomerLocator.inputSearchGroup),group);
+        Thread.sleep(1000);
+        Common.click(By.xpath("//div[@id='bs-select-1']/descendant::span[contains(normalize-space(),'"+group+"')]"));
+        driver.findElement(By.xpath("//body")).click();
+
+        Actions action = new Actions(driver);
+        action.moveToElement(driver.findElement(By.xpath(AddCustomerLocator.countryDropdown))).perform();
+
+        Common.click(By.xpath(AddCustomerLocator.currencyDropdown));
+        Common.click(By.xpath(AddCustomerLocator.currencyOptionUSD));
+        Common.click(By.xpath(AddCustomerLocator.defaultLanguageDropdown));
+        Common.click(By.xpath(AddCustomerLocator.defaultLanguageEnglish));
+        Common.sendKey(By.xpath(AddCustomerLocator.addressTextArea),"8414 Martin Luther King Jr Way South Seattle Washington 98118");
+        Common.sendKey(By.xpath(AddCustomerLocator.inputCity),"Washington ");
+        Common.sendKey(By.xpath(AddCustomerLocator.inputState),"Seattle ");
+        Common.sendKey(By.xpath(AddCustomerLocator.inputZipCode),"98118");
+        Common.click(By.xpath(AddCustomerLocator.countryDropdown));
+        Common.click(By.xpath(AddCustomerLocator.countryOption));
+
+        action.moveToElement(driver.findElement(By.xpath(AddCustomerLocator.btnOnlySave))).perform();
+        Common.click(By.xpath(AddCustomerLocator.btnSaveAndCreateContract));
+        Thread.sleep(3000);
+        Boolean newContactPopup = driver.findElement(By.xpath("//div[@id='contact']")).isDisplayed();
+        Assert.assertTrue(newContactPopup,"Add new contact pop-up is not shown");
+        Common.click(By.xpath("//button[normalize-space() = 'Close']"));
+        Boolean companyNameDisplay = driver.findElement(By.xpath("//span[contains(text(),'"+companyname+"')]")).isDisplayed();
+        Assert.assertTrue(companyNameDisplay,"Add customer not successfully");
     }
 
     @Test
